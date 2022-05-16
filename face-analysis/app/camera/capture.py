b20026b2f862ee6bf6083613f76699d0aa049a85
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-05-16 12:58:56 -0300

    feat: added cv analysis for face

--- face-analysis/app/camera/capture.py
+++ face-analysis/app/camera/capture.py
@@ -39,8 +39,21 @@ class CameraCapture:
             image = resize(frame, width=720)
             gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+            face = self.__cascade_face.detectMultiScale(gray_image, 1.3, 5)
+            smile = self.__cascade_smile.detectMultiScale(gray_image, 1.3, 20)
+            eye = self.__cascade_eye.detectMultiScale(gray_image, 1.3, 1)
+
+            for (x,y,w,h) in face:
+                cv2.rectangle(image,(x,y),(x + w,y + h),(255,0,0),2)
+                for (xO,yO,wO,hO) in eye:
+                    if( (x <= xO) and (y <= yO) and ( x + w >= xO + wO) and ( y + h >= yO + hO)):
+                        cv2.rectangle(image, (xO,yO),(xO + wO,yO + hO),(255,255,255),2)
+                for (xS,yS,wS,hS) in smile:
+                    if( (x <= xS) and (y <= yS) and ( x + w >= xS + wS) and ( y + h >= yS + hS)):
+                        cv2.rectangle(image, (xS, yS),(xS + wS, yS + hS),(0,255,0),2)            
+
             cv2.imshow("Image Captured", image)
-            cv2.imshow("Image Captured", gray_image)
+            cv2.imshow("Image Captured Gray", gray_image)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 print("[INFO] Detector was been paused!")
