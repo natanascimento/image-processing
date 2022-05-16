4b29b6b0806b6887c58eff9b417a7a10c73bd26f
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-05-16 13:00:57 -0300

    feat: added cv analysis for face (#7)

--- face-analysis/app/__init__.py
+++ face-analysis/app/__init__.py
@@ -1,9 +1,5 @@
 from app.camera.capture import CameraCapture
-from app.core.config import settings
 
 
 def main():
-  print(settings.HAARCASCADE_EYE)
-  print(settings.HAARCASCADE_FACE)
-  print(settings.HAARCASCADE_SMILE)
-  #CameraCapture().run()
+  CameraCapture().run()
